10ca216093877fd08ea5b1a8952c4a59ff621a9f
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-03-01 22:25:13 +0800

    Create main4.c

---
+++ HW1/main4.c
@@ -0,0 +1,14 @@
+void filter(char *a){
+    int len = 0;
+    int n=0;
+    while (a[len] != '\0') {len++;}
+    for(int i=0; i<len-1; i++){
+        if(a[i]<='9'){printf("%c",a[i]);}
+    }
+}
+
+int main()
+{
+    char a[]="x-99acbc";
+    filter(a);
+}
