4299973058add872c4029606983d17d62504fe17
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-03-09 14:14:28 +0800

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-    FILE* fp;
-    int arr_write[5] = {1,2,3,4,5};
-    int arr_read[5];
+
+int main(){
+    int a[]={1,2,3};
+    char b[]="ABC";
+    float c[] = {1.1, 1.2, 1.3 };
+    FILE*fp;
     if((fp=fopen("a.bin","wb+"))==NULL){
         printf("Cannot open the file...");
         exit(1);
     }
-    if (fwrite(arr_write,sizeof(arr_write),1,fp)!=1){
+    if(fwrite(a,sizeof(a),1,fp)!=1){
         printf("File write erro...\n");
     }
     fseek(fp,0,SEEK_SET);
     
-    if(fread(arr_write,sizeof(arr_write),1,fp)!=1){
-        printf("File read reeo...\n");
+    if(fread(a,sizeof(a),1,fp)!=1){
+        printf("File read erro...\n");
     }
+    //-----------------------------------
+    if((fp=fopen("a.bin","wb+"))==NULL){
+        printf("Cannot open the file...");
+        exit(1);
+    }
+    if(fwrite(b,sizeof(b),1,fp)!=1){
+        printf("File write erro...\n");
+    }
+    fseek(fp,0,SEEK_SET);
     
+    if(fread(b,sizeof(b),1,fp)!=1){
+        printf("File read erro...\n");
+    }
+    //-----------------------------------
+    if((fp=fopen("a.bin","wb+"))==NULL){
+        printf("Cannot open the file...");
+        exit(1);
+    }
+    if(fwrite(c,sizeof(c),1,fp)!=1){
+        printf("File write erro...\n");
+    }
+    fseek(fp,0,SEEK_SET);
+    
+    if(fread(c,sizeof(c),1,fp)!=1){
+        printf("File read erro...\n");
+    }
+
     return 0;
 }
