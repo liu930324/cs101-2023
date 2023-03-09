a7b46ea1477663edeb20fc3237e98fcc3c8b5e0d
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-03-09 13:51:03 +0800

    Create main.c

---
+++ lab1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a[] ={1,2,3};
+    char b[] ="ABC";
+    float c[] ={1.1,1.2,1.3};
+    FILE* fp;
+    int arr_write[5] = {1,2,3,4,5};
+    int arr_read[5];
+
+    return 0;
+}
