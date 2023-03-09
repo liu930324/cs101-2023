0b8b2697fb161591dd75aa63f81724dff0e29590
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-03-09 14:05:52 +0800

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 int main()
 {
-    int a[] ={1,2,3};
-    char b[] ="ABC";
-    float c[] ={1.1,1.2,1.3};
     FILE* fp;
+    int arr_write[5] = {1,2,3,4,5};
+    int arr_read[5]
     if((fp=fopen("a.bin","wb+"))==NULL){
         printf("Cannot open the file...");
         exit(1);
