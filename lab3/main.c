f207ff9d70a6c068608efed669115cb19bc6f1e0
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-03-16 14:59:49 +0800

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -1,1 +1,18 @@
+typedf struct person{
+    int id;
+    char first_name[32];
+    char last_name[32];
+}person_t
 
+int main(){
+    FILE* rfp + fopen("person.bin","r");
+    person_t tmp[1];
+    int i=0;
+    while(fread(tmp,sizeof(person_t), 1, rfp)){
+        printf("[%d] %d: %s %s\n",i,
+                                    tmp[0].id,
+                                    tmp[0].first_name,
+                                    tmp[0].last_name);
+        i++
+    }
+    fclose(rfp);
